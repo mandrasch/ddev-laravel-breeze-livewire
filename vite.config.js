b785b2741ea987135223de2b6adcb5006dd7e3c1
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-05-19 11:27:40 +0200

    refactor: clean up vite.config.hs

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     server: {
-        // respond to all network requests (same as '0.0.0.0')
-        host: true,
-        // we need a strict port to match on PHP side
+        host: true, // respond to all network requests
         strictPort: true,
         port: 5173,
         hmr: {
-            // Force the Vite client to connect via SSL,
-            // this will also force a "https://" URL in the public/hot file
             protocol: 'wss',
-            // The host where the Vite dev server can be accessed
-            // This will also force this host to be written to the public/hot file
-            host: `${process.env.DDEV_HOSTNAME}`
+            host: `${process.env.DDEV_HOSTNAME}`,
         }
     },
 });
